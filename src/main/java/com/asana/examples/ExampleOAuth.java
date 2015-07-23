40e25195e165595c9daeed7fae5de2b25e954de6
Erik Schultink <erik@engetc.com>
2015-07-23 15:19:23 -0400

    make ExampleOAuth work for users w/o photos ...

--- src/main/java/com/asana/examples/ExampleOAuth.java
+++ src/main/java/com/asana/examples/ExampleOAuth.java
@@ -65,7 +65,9 @@ public class ExampleOAuth
         User user = client.users.me().execute();
         System.out.println("me=" + user.name);
         System.out.println(user.id);
-        System.out.println(user.photo.image_128x128);
+        
+        if (user.photo != null)
+            System.out.println(user.photo.image_128x128);
         System.out.println(user.workspaces.iterator().next().name);
 
         // demonstrate creating a client using a previously obtained bearer token
