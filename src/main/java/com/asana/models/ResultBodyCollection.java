5a723b1df04d6895aff9a87b629f87c6b0af51ee
Erik Schultink <erik@worklytics.co>
2016-11-22 18:27:33 -0500

    make NextPage static so Gson can construct it

--- src/main/java/com/asana/models/ResultBodyCollection.java
+++ src/main/java/com/asana/models/ResultBodyCollection.java
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
 public class ResultBodyCollection<T> {
-    public class NextPage {
+    public static class NextPage {
         public String offset;
         public String path;
         public String uri;
