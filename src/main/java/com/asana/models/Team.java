f4d7c730c1e9a4fb740713d185169d59b26d5598
Joe Trollo <joetrollo@asana.com>
2018-08-06 15:53:07 -0700

    Add description to team

--- src/main/java/com/asana/models/Team.java
+++ src/main/java/com/asana/models/Team.java
@@ -1,7 +1,13 @@
 package com.asana.models;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Team extends Resource {
     public String name;
 
     public Workspace organization;
+
+    public String description;
+    @SerializedName("html_description")
+    public String htmlDescription;
 }
