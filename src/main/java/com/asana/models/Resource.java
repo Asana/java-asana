80a0b49095e47d690790053189e7556c3ccd69a8
Florian Schoppmann <mail@florian-schoppmann.net>
2019-03-06 23:05:37 -0800

    Add resource_type and resource_subtype
    See this announcement:
    https://forum.asana.com/t/new-task-subtypes-and-better-type-information/32047

--- src/main/java/com/asana/models/Resource.java
+++ src/main/java/com/asana/models/Resource.java
@@ -1,5 +1,12 @@
 package com.asana.models;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Resource {
     public String id;
+
+    @SerializedName("resource_type")
+    public String resourceType;
+    @SerializedName("resource_subtype")
+    public String resourceSubtype;
 }
