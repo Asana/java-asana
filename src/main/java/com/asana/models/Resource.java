76351b615d0d643b09848800bcb2fa0c95b44749
Ross Grambo <rossgrambo@asana.com>
2020-07-17 12:02:27 -0700

    removed id from resources

--- src/main/java/com/asana/models/Resource.java
+++ src/main/java/com/asana/models/Resource.java
@@ -3,8 +3,6 @@ package com.asana.models;
 import com.google.gson.annotations.SerializedName;
 
 public class Resource {
-    public String id;
-
     public String gid;
 
     @SerializedName("resource_type")
