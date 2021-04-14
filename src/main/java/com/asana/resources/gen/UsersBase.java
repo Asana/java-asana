aa6839afa2e4fd82ea70aeb8f244b7f9e832bd61
Ross Grambo <rossgrambo@asana.com>
2021-04-14 16:56:08 -0700

    Generated from OpenAPI

--- src/main/java/com/asana/resources/gen/UsersBase.java
+++ src/main/java/com/asana/resources/gen/UsersBase.java
@@ -46,7 +46,7 @@ import java.util.List;
         }
         /**
         * Get a user
-        * Returns the full user record for the single user with the provided ID. Results are sorted by user ID.
+        * Returns the full user record for the single user with the provided ID.
             * @param userGid A string identifying a user. This can either be the string \&quot;me\&quot;, an email, or the gid of a user. (required)
             * @param optFields Defines fields to return. Some requests return *compact* representations of objects in order to conserve resources and complete the request more efficiently. Other times requests return more information than you may need. This option allows you to list the exact set of fields that the API should be sure to return for the objects. The field names should be provided as paths, described below. The id of included objects will always be returned, regardless of the field options. (optional)
             * @param optPretty Provides “pretty” output. Provides the response in a “pretty” format. In the case of JSON this means doing proper line breaking and indentation to make it readable. This will take extra time and increase the response size so it is advisable only to use this during debugging. (optional)
@@ -69,6 +69,7 @@ import java.util.List;
         /**
         * Get multiple users
         * Returns the user records for all users in all workspaces and organizations accessible to the authenticated user. Accepts an optional workspace ID parameter. Results are sorted by user ID.
+            * @param team The team ID to filter users on. (optional)
             * @param workspace The workspace or organization ID to filter users on. (optional)
             * @param offset Offset token. An offset to the next page returned by the API. A pagination request will return an offset token, which can be used as an input parameter to the next request. If an offset is not passed in, the API will return the first page of results. &#x27;Note: You can only pass in an offset that was returned to you via a previously paginated request.&#x27; (optional)
             * @param limit Results per page. The number of objects to return per page. The value must be between 1 and 100. (optional)
@@ -77,11 +78,12 @@ import java.util.List;
         * @return CollectionRequest(User)
         * @throws IOException If we fail to call the API, e.g. server error or cannot deserialize the response body
         */
-        public CollectionRequest<User> getUsers(String workspace, String offset, Integer limit, List<String> optFields, Boolean optPretty) throws IOException {
+        public CollectionRequest<User> getUsers(String team, String workspace, String offset, Integer limit, List<String> optFields, Boolean optPretty) throws IOException {
             String path = "/users";
 
             CollectionRequest<User> req = new CollectionRequest<User>(this, User.class, path, "GET")
                 .query("workspace", workspace)
+                .query("team", team)
                 .query("opt_pretty", optPretty)
                 .query("opt_fields", optFields)
                 .query("limit", limit)
@@ -90,59 +92,55 @@ import java.util.List;
             return req;
         }
 
-        public CollectionRequest<User> getUsers(String workspace) throws IOException {
-            return getUsers(workspace, null, (int)Client.DEFAULTS.get("page_size"), null, false);
+        public CollectionRequest<User> getUsers(String team, String workspace) throws IOException {
+            return getUsers(team, workspace, null, (int)Client.DEFAULTS.get("page_size"), null, false);
         }
         /**
         * Get users in a team
-        * Returns the compact records for all users that are members of the team.
+        * Returns the compact records for all users that are members of the team. Results are sorted alphabetically and limited to 2000. For more results use the &#x60;/users&#x60; endpoint.
             * @param teamGid Globally unique identifier for the team. (required)
             * @param offset Offset token. An offset to the next page returned by the API. A pagination request will return an offset token, which can be used as an input parameter to the next request. If an offset is not passed in, the API will return the first page of results. &#x27;Note: You can only pass in an offset that was returned to you via a previously paginated request.&#x27; (optional)
-            * @param limit Results per page. The number of objects to return per page. The value must be between 1 and 100. (optional)
             * @param optFields Defines fields to return. Some requests return *compact* representations of objects in order to conserve resources and complete the request more efficiently. Other times requests return more information than you may need. This option allows you to list the exact set of fields that the API should be sure to return for the objects. The field names should be provided as paths, described below. The id of included objects will always be returned, regardless of the field options. (optional)
             * @param optPretty Provides “pretty” output. Provides the response in a “pretty” format. In the case of JSON this means doing proper line breaking and indentation to make it readable. This will take extra time and increase the response size so it is advisable only to use this during debugging. (optional)
         * @return CollectionRequest(User)
         * @throws IOException If we fail to call the API, e.g. server error or cannot deserialize the response body
         */
-        public CollectionRequest<User> getUsersForTeam(String teamGid, String offset, Integer limit, List<String> optFields, Boolean optPretty) throws IOException {
+        public CollectionRequest<User> getUsersForTeam(String teamGid, String offset, List<String> optFields, Boolean optPretty) throws IOException {
             String path = "/teams/{team_gid}/users".replace("{team_gid}", teamGid);
 
             CollectionRequest<User> req = new CollectionRequest<User>(this, User.class, path, "GET")
                 .query("opt_pretty", optPretty)
                 .query("opt_fields", optFields)
-                .query("limit", limit)
                 .query("offset", offset);
 
             return req;
         }
 
         public CollectionRequest<User> getUsersForTeam(String teamGid) throws IOException {
-            return getUsersForTeam(teamGid, null, (int)Client.DEFAULTS.get("page_size"), null, false);
+            return getUsersForTeam(teamGid, null, null, false);
         }
         /**
         * Get users in a workspace or organization
-        * Returns the user records for all users in the specified workspace or organization. Results are sorted alphabetically by user names.
+        * Returns the compact records for all users in the specified workspace or organization. Results are sorted alphabetically and limited to 2000. For more results use the &#x60;/users&#x60; endpoint.
             * @param workspaceGid Globally unique identifier for the workspace or organization. (required)
             * @param offset Offset token. An offset to the next page returned by the API. A pagination request will return an offset token, which can be used as an input parameter to the next request. If an offset is not passed in, the API will return the first page of results. &#x27;Note: You can only pass in an offset that was returned to you via a previously paginated request.&#x27; (optional)
-            * @param limit Results per page. The number of objects to return per page. The value must be between 1 and 100. (optional)
             * @param optFields Defines fields to return. Some requests return *compact* representations of objects in order to conserve resources and complete the request more efficiently. Other times requests return more information than you may need. This option allows you to list the exact set of fields that the API should be sure to return for the objects. The field names should be provided as paths, described below. The id of included objects will always be returned, regardless of the field options. (optional)
             * @param optPretty Provides “pretty” output. Provides the response in a “pretty” format. In the case of JSON this means doing proper line breaking and indentation to make it readable. This will take extra time and increase the response size so it is advisable only to use this during debugging. (optional)
         * @return CollectionRequest(User)
         * @throws IOException If we fail to call the API, e.g. server error or cannot deserialize the response body
         */
-        public CollectionRequest<User> getUsersForWorkspace(String workspaceGid, String offset, Integer limit, List<String> optFields, Boolean optPretty) throws IOException {
+        public CollectionRequest<User> getUsersForWorkspace(String workspaceGid, String offset, List<String> optFields, Boolean optPretty) throws IOException {
             String path = "/workspaces/{workspace_gid}/users".replace("{workspace_gid}", workspaceGid);
 
             CollectionRequest<User> req = new CollectionRequest<User>(this, User.class, path, "GET")
                 .query("opt_pretty", optPretty)
                 .query("opt_fields", optFields)
-                .query("limit", limit)
                 .query("offset", offset);
 
             return req;
         }
 
         public CollectionRequest<User> getUsersForWorkspace(String workspaceGid) throws IOException {
-            return getUsersForWorkspace(workspaceGid, null, (int)Client.DEFAULTS.get("page_size"), null, false);
+            return getUsersForWorkspace(workspaceGid, null, null, false);
         }
     }
